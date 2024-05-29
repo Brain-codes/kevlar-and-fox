7c71938a6afe5fc9e7a3a0db4bc1d9c7861a0785
adenuga adewumi <adenugaadewumi01@gmail.com>
2024-05-29 14:16:01 +0100

    experties fixed

--- src/components/tiles/ExpertiesTiles.tsx
+++ src/components/tiles/ExpertiesTiles.tsx
@@ -21,11 +21,9 @@ const ExpertiesTiles = ({
         {title}
       </h1>
       <div
-        className={`${
-          imageLeft ? "md:pr-[5%] pr-0" : "md:pl-[5%] pl-0 justify-between"
-        }  flex md:flex-row flex-col md:gap-0 gap-20`}
+        className={`  flex md:flex-row flex-col md:gap-0 gap-20 md:pl-[5%] pl-0 justify-between`}
       >
-        {imageLeft && (
+        {/* {imageLeft && (
           <div className="md:w-[45%] w-full">
             <Image
               src={image}
@@ -36,34 +34,34 @@ const ExpertiesTiles = ({
               className="w-full object-contain"
             />
           </div>
-        )}
+        )} */}
         <div className="md:w-[45%] w-[91%] flex flex-col md:mx-0 mx-[5%] md:gap-0 gap-10 justify-between">
           {items.map((data, index) => (
-            <div key={index}>
+            <div key={index} className="mb-5">
               <div className="flex gap-3 items-center ">
                 <StarIcon className=" w-[23px] h-[23px]" />
                 <h1 className="font-[800] md:text-[22px] text-[18px] md:leading-light leading-[21px]">
                   {data.title}
                 </h1>
               </div>
-              <p className="font-[400] md:text-[17px] text-[12px] md:mt-0 mt-3">
+              <p className="font-[400] md:text-[17px] text-[12px] md:mt-3 mt-3 text-justify">
                 {data.description}
               </p>
             </div>
           ))}
         </div>
-        {!imageLeft && (
-          <div className="md:w-[45%] w-full ">
-            <Image
-              src={image}
-              alt="Public Relations"
-              width={1000}
-              height={1000}
-              priority
-              className="w-full object-contain"
-            />
-          </div>
-        )}
+        <div className="md:w-[45%] w-full ">
+          <Image
+            src={image}
+            alt="Public Relations"
+            width={1000}
+            height={1000}
+            priority
+            className="w-full object-contain"
+          />
+        </div>
+        {/* {!imageLeft && (
+        )} */}
       </div>
     </div>
   );
