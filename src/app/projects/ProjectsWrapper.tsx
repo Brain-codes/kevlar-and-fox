6d7583591247b7f690d4e570b30378710708c72d
adenuga adewumi <adenugaadewumi01@gmail.com>
2024-05-30 10:40:11 +0100

    dual navbar and footer fixed

--- src/app/projects/ProjectsWrapper.tsx
+++ src/app/projects/ProjectsWrapper.tsx
@@ -1,20 +1,13 @@
 "use client";
 import CaseStudy from "@/components/CaseStudy";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import React from "react";
 
 const ProjectsWrapper = () => {
   return (
     <>
-      {/* <ParallaxProvider> */}
-      {/* <CustomCursor /> */}
-      <Navbar />
       <main className="flex flex-col whole-cont">
         <CaseStudy id="case-studies" />
       </main>
-      <Footer />
-      {/* </ParallaxProvider> */}
     </>
   );
 };
